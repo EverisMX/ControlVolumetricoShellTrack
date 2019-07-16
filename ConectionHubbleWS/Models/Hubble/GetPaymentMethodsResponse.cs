7e264d850b0cc7bcbf466ba7b109c06f778e2708
Juan Manuel Alvarez Hernandez <jalvareh@everis.com>
2019-07-15 21:37:06 -0500

    SHLMX WS - Sumary hora

---
+++ ConectionHubbleWS/Models/Hubble/GetPaymentMethodsResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conection.HubbleWS
+{
+    public class GetPaymentMethodsResponse : BaseResponse
+    {
+        public IList<PaymentMethod> PaymentMethodList { get; set; } = new List<PaymentMethod>();
+
+        /// <summary>
+        /// Identificador de la tarjeta bancaria por defecto.
+        /// Nulo si no hay ninguna
+        /// </summary>
+        public string DefaultBankCardId { get; set; }
+    }
+}
